cf1b31536ea4a6bab0f39c3b11c5eea9a5970650
samuelebompani <samuelebompani@gmail.com>
2023-04-03 17:24:23 +0200

    feat: created welcome activity

---
+++ app/src/main/java/com/example/godstradeoffs/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package com.example.godstradeoffs;
+
+import androidx.appcompat.app.AppCompatActivity
+
+public class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstancesState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
